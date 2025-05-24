c437e5823ec1eed95f5e007a07262bdab6f5c82e
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-05-24 19:42:37 +0700

    Add Domain Event Dispatcher Implementation
    Add MediatR implementation of domain event dispatcher.

---
+++ RustRetail.SharedApplication/Behaviors/Event/MediatRDomainEventDispatcher.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using RustRetail.SharedApplication.Abstractions;
+using RustRetail.SharedKernel.Domain.Events.Domain;
+
+namespace RustRetail.SharedApplication.Behaviors.Event
+{
+    public class MediatRDomainEventDispatcher(IMediator mediator)
+        : IDomainEventDispatcher
+    {
+        public async Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents,
+            CancellationToken cancellationToken = default)
+        {
+            foreach (var domainEvent in domainEvents)
+            {
+                var notificationType = typeof(DomainEventNotification<>).MakeGenericType(domainEvent.GetType());
+                var notification = Activator.CreateInstance(notificationType, domainEvent);
+                await mediator.Publish((INotification)notification!, cancellationToken);
+            }
+        }
+    }
+}
