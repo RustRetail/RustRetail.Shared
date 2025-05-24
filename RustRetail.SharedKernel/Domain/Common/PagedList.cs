7ef89183436fd442144fd9eeb88a07711281f76a
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-05-25 03:54:01 +0700

    Add Validations to PagedList

--- RustRetail.SharedKernel/Domain/Common/PagedList.cs
+++ RustRetail.SharedKernel/Domain/Common/PagedList.cs
@@ -31,6 +31,8 @@
             int pageNumber,
             int pageSize)
         {
+            ValidatePageNavigations(pageNumber, pageSize);
+
             int totalCount = query.Count();
             var items = query
                 .Skip((pageNumber - 1) * pageSize)
@@ -45,7 +47,48 @@
             int pageSize,
             int totalCount)
         {
+            ValidatePageNavigations(pageNumber, pageSize);
+            ValidateItemsAndTotalCount(items, totalCount);
+
             return new(items, pageNumber, pageSize, totalCount);
+        }
+
+        private static void ValidatePageNavigations(int pageNumber, int pageSize)
+        {
+            if (pageNumber <= 0)
+            {
+                throw new ArgumentException("Page number must be larger than 0.");
+            }
+            if (pageSize <= 0)
+            {
+                throw new ArgumentException("Page size must be larger than 0.");
+            }
+        }
+
+        private static void ValidateItemsAndTotalCount(List<T> items, int totalCount)
+        {
+            if (totalCount < 0)
+            {
+                throw new ArgumentException("Total item count must be equal or larger than 0.");
+            }
+            if (items != null)
+            {
+                if (items.Count > 0 && totalCount <= 0)
+                {
+                    throw new InvalidOperationException("Total item count must be larger than 0 when the items list contains element(s).");
+                }
+                if (items.Count == 0 && totalCount > 0)
+                {
+                    throw new InvalidOperationException("Total item count must be 0 when the items list contains no element.");
+                }
+            }
+            else
+            {
+                if (totalCount > 0)
+                {
+                    throw new ArgumentException("Total item count must be 0 when the item list is null.");
+                }
+            }
         }
     }
 }
