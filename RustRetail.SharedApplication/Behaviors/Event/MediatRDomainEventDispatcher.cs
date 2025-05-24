9435286184297ca177a49c1c1227ebbe06184bbc
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-05-25 00:32:12 +0700

    Modify Domain Event Dispatcher
    Add new method to domain event dispatcher to dispatch single event. Change dependency type of MediaR domain event dispatcher to IPublisher.

--- RustRetail.SharedApplication/Behaviors/Event/MediatRDomainEventDispatcher.cs
+++ RustRetail.SharedApplication/Behaviors/Event/MediatRDomainEventDispatcher.cs
@@ -4,17 +4,24 @@ using RustRetail.SharedKernel.Domain.Events.Domain;
 
 namespace RustRetail.SharedApplication.Behaviors.Event
 {
-    public class MediatRDomainEventDispatcher(IMediator mediator)
+    public class MediatRDomainEventDispatcher(IPublisher publisher)
         : IDomainEventDispatcher
     {
-        public async Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents,
+        public async Task DispatchAsync(IDomainEvent domainEvent, CancellationToken cancellationToken = default)
+        {
+            var notificationType = typeof(DomainEventNotification<>).MakeGenericType(domainEvent.GetType());
+            var notification = Activator.CreateInstance(notificationType, domainEvent);
+            await publisher.Publish((INotification)notification!, cancellationToken);
+        }
+
+        public async Task DispatchMultipleAsync(IEnumerable<IDomainEvent> domainEvents,
             CancellationToken cancellationToken = default)
         {
             foreach (var domainEvent in domainEvents)
             {
                 var notificationType = typeof(DomainEventNotification<>).MakeGenericType(domainEvent.GetType());
                 var notification = Activator.CreateInstance(notificationType, domainEvent);
-                await mediator.Publish((INotification)notification!, cancellationToken);
+                await publisher.Publish((INotification)notification!, cancellationToken);
             }
         }
     }
--- RustRetail.SharedKernel/Domain/Events/Domain/IDomainEventDispatcher.cs
+++ RustRetail.SharedKernel/Domain/Events/Domain/IDomainEventDispatcher.cs
@@ -2,6 +2,7 @@
 {
     public interface IDomainEventDispatcher
     {
-        Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
+        Task DispatchMultipleAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
+        Task DispatchAsync(IDomainEvent domainEvent, CancellationToken cancellationToken = default);
     }
 }
