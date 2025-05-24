8daebdf9dedabaeaf1f4a6d2855db4d1b1181a3c
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-05-24 19:35:25 +0700

    Add Domain Event Dispatcher Interface

---
+++ RustRetail.SharedKernel/Domain/Events/Domain/IDomainEventDispatcher.cs
@@ -0,0 +1,7 @@
+﻿namespace RustRetail.SharedKernel.Domain.Events.Domain
+{
+    public interface IDomainEventDispatcher
+    {
+        Task DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken cancellationToken = default);
+    }
+}
