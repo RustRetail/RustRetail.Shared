e0b889360caeb8c0fec2fc2258ca766fe6bf4e33
Nguyễn Thái Hoà <hoa41300@gmail.com>
2025-06-20 11:55:23 +0700

    Add SharedContracts Project
    Add SharedContracts project with purpose of sharing contracts (request/reponse, integration event,...) between services.

---
+++ src/RustRetail.SharedContracts/IntegrationEvents/Abstractions/IntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿using RustRetail.SharedContracts.IntegrationEvents.Abstractions;
+
+namespace RustRetail.SharedKernel.Domain.Events.Integration
+{
+    /// <summary>
+    /// Represents a base class for integration events.
+    /// </summary>
+    public abstract class IntegrationEvent : IIntegrationEvent
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+
+        public DateTime OccurredOn { get; } = DateTime.UtcNow;
+    }
+}
---
+++ src/RustRetail.SharedContracts/IntegrationEvents/IdentityService/User/UserRegisteredEvent.cs
@@ -0,0 +1,18 @@
+﻿using RustRetail.SharedKernel.Domain.Events.Integration;
+
+namespace RustRetail.SharedContracts.IntegrationEvents.IdentityService.User
+{
+    public sealed class UserRegisteredEvent : IntegrationEvent
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+
+        public UserRegisteredEvent(Guid userId, string userName, string email)
+        {
+            UserId = userId;
+            UserName = userName;
+            Email = email;
+        }
+    }
+}
