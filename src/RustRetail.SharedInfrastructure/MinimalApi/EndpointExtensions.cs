588cfcee5c247da080852e2d6a7b184d8deab07c
Nguyễn Thái Hòa <hoa41300@gmail.com>
2025-06-22 20:30:14 +0700

    Update SharedInfrastructure
    Add XML comments and update package version of SharedInfrastructure project.

--- src/RustRetail.SharedInfrastructure/MinimalApi/EndpointExtensions.cs
+++ src/RustRetail.SharedInfrastructure/MinimalApi/EndpointExtensions.cs
@@ -6,8 +6,17 @@ using System.Reflection;
 
 namespace RustRetail.SharedInfrastructure.MinimalApi
 {
+    /// <summary>
+    /// Provides extension methods for registering and mapping minimal API endpoints.
+    /// </summary>
     public static class EndpointExtensions
     {
+        /// <summary>
+        /// Registers all non-abstract, non-interface types implementing <see cref="IEndpoint"/> from the specified assembly as transient services.
+        /// </summary>
+        /// <param name="services">The service collection to add the endpoints to.</param>
+        /// <param name="assembly">The assembly to scan for <see cref="IEndpoint"/> implementations.</param>
+        /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
         public static IServiceCollection AddEndpoints(
             this IServiceCollection services,
             Assembly assembly)
@@ -24,6 +33,14 @@ namespace RustRetail.SharedInfrastructure.MinimalApi
             return services;
         }
 
+        /// <summary>
+        /// Maps all registered <see cref="IEndpoint"/> instances to the application's endpoint route builder.
+        /// </summary>
+        /// <param name="app">The web application to map endpoints for.</param>
+        /// <param name="routeGroupBuilder">
+        /// An optional <see cref="RouteGroupBuilder"/> to group endpoints; if <c>null</c>, endpoints are mapped directly to the application.
+        /// </param>
+        /// <returns>The <see cref="IApplicationBuilder"/> for chaining.</returns>
         public static IApplicationBuilder MapEndpoints(
             this WebApplication app,
             RouteGroupBuilder? routeGroupBuilder = null)
--- src/RustRetail.SharedInfrastructure/MinimalApi/IEndpoint.cs
+++ src/RustRetail.SharedInfrastructure/MinimalApi/IEndpoint.cs
@@ -2,8 +2,15 @@
 
 namespace RustRetail.SharedInfrastructure.MinimalApi
 {
+    /// <summary>
+    /// Defines a contract for mapping minimal API endpoints to an endpoint route builder.
+    /// </summary>
     public interface IEndpoint
     {
+        /// <summary>
+        /// Maps the endpoint to the specified <see cref="IEndpointRouteBuilder"/>.
+        /// </summary>
+        /// <param name="app">The endpoint route builder to which the endpoint will be mapped.</param>
         void MapEndpoint(IEndpointRouteBuilder app);
     }
 }
